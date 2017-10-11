a58c2ecbb2b2a4a4d5721fb37f5c13582fe67292
Malcolm Groves <macgroves@gmail.com>
2017-10-11 17:34:41 -0400

    Commented Guess and MasterAlg classes

--- src/game/Guess.java
+++ src/game/Guess.java
@@ -1,11 +1,19 @@
 package game;
 
+/**
+ * Represents a mastermind code Guess. Contains methods
+ * to store and manipulate an int[] code array.
+ * @author Malcolm Groves
+ *
+ */
 public class Guess {
 	
 	private int[] code;
 	
-	/*
-	 * Constructor
+	/**	
+	 * Constructs a Guess object that holds a mastermind code.
+	 * @param	code	An integer array that represents
+	 * 					the mastermind code
 	 */
 	public Guess(int[] code) {
 		this.code = new int[code.length];
@@ -14,8 +22,10 @@ public class Guess {
 		}
 	}
 	
-	/*
-	 * Copy constructor
+	/** Constructs a copy of a Guess object. 
+	 * 
+	 * @param 	guess	A guess object that serves as a 
+	 * 					template for a deep copy
 	 */
 	public Guess(Guess guess) {
 		this(guess.get_code());
@@ -23,7 +33,14 @@ public class Guess {
 	
 	
 	
-	
+	/**
+	 * Checks Guess equality. Overrides the native Object.equals() method.
+	 * Compares <code> this </code> to another Object that is caste as a
+	 * Guess object.
+	 * @param 	obj Object that can be caste to a Guess object.
+	 * @return 	boolean value that is true if the codes are the same and
+	 * 			false otherwise.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		
@@ -41,22 +58,39 @@ public class Guess {
 		return true;
 	}
 	
+	
+	/**
+	 * Computes hash code for the Guess object. Treats the code as
+	 * a base 10 integer so that if the code is [1, 4, 2, 3] the function
+	 * returns 1423. Overrides the native hashCode() Object method.
+	 * @return A hash code integer.
+	 */
 	@Override
 	public int hashCode() {
+		
 		int hash_code = 0;
-		//System.out.println("First: " + String.valueOf(code[0]));
+		
 		for(int i = code.length - 1; i >= 0; --i) {
 			int radix = code[i] * (int) Math.pow(10, i);
 			hash_code += radix;
 		}
-		//System.out.println(hash_code);
+		
 		return hash_code;
 	}
 	
+	/**
+	 * Gets the Guess int code array.
+	 * @return	int code array
+	 */
 	public int[] get_code() {
 		return code;
 	}
 	
+	/**
+	 * Converts the Guess code to a string. To be used for
+	 * printing the code.
+	 * @return String code
+	 */
 	public String to_string() {
 		
 		String output = "";
--- src/game/MasterAlg.java
+++ src/game/MasterAlg.java
@@ -5,8 +5,16 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.ArrayList;
 
+/**
+ * An algorithm that can solve mastermind in 5 moves or less.
+ * Implements Knuth's minimax algorithm for solving the np-complete
+ * mastermind problem.
+ * Contains a HashSet to store all guess objects.
+ * @author mac2
+ */
 public class MasterAlg {
 
+	// [0, 0, 1, 1] is shown to be the most effective first guess
 	private static final int[] FIRST_GUESS = {0, 0, 1, 1};
 	private static final int NUM_POSITIONS = 4;
 	private static final int NUM_COLORS = 6;
@@ -17,35 +25,46 @@ public class MasterAlg {
 	private HashSet<Guess> set;
 	private Guess curr;
 
-	// constructor for the algorithm
+	/**
+	 * Constructs a MasterAlg object and initializes instances of
+	 * set, curr, and instantiates the set.
+	 */
 	public MasterAlg() {
 		set = new HashSet<Guess>();
 		curr = new Guess(FIRST_GUESS);
 		instantiate_set();
 	}
 
-	// give the first guess
+	/**
+	 * Gets the first guess.
+	 * @return	First Guess int[] code
+	 */
 	public int[] first_guess() {
 		return FIRST_GUESS;
 	}
 
-	// generate the next guess, give the code of the previous guess
+	/**
+	 * Generates the next Guess for the MasterAlg. Given a grade,
+	 * iterates through all remaining possible Guess objects and removes 
+	 * all Guess objects that, if true, would not give the same
+	 * grade to the current Guess.
+	 * @param 	grade	grade[0] is the number of pins with the correct
+	 * 					color and position. grade[1] is the number of pins
+	 * 					with the correct color, but incorrect position.
+	 * @return	int[] code that has the maximum minimum elimination count
+	 * 			in the set of possible guesses.
+	 */
 	public int[] next_guess(int[] grade) {
 
 		Iterator<Guess> iter = set.iterator();
 		ArrayList<Guess> to_remove = new ArrayList<Guess>();
 
-		
-		
-		// iterate through set of possibilities
 		while(iter.hasNext()) {
-
 			
 			Guess next = iter.next();
-			//Guess copy = new Guess(next);
-
+			
 			// if a possibility is true, it would give the given grade to the curr
-			if(!equal(grade, calc_grade(next, curr))) {
+			if(!equal_grade(grade, calc_grade(next, curr))) {
 				to_remove.add(next);
 			}
 		}
@@ -55,18 +74,15 @@ public class MasterAlg {
 			set.remove(g);
 		}
 		
-		// reset iterator
 		iter = set.iterator();
 		
 		Guess next_guess = null;
 		int max_elim = -1;
 		
-
 		// iterate through and count eliminations for each
 		while(iter.hasNext()) {
-			// calculate the number of possibilities that could be eliminated by the code
-			Guess next = iter.next();
 			
+			Guess next = iter.next();
 			
 			int elim_count = count_elim(next);
 			
@@ -81,6 +97,13 @@ public class MasterAlg {
 		return curr.get_code();
 	}
 
+	/**
+	 * Counts the minimum number of Guess objects in the set of all remaining
+	 * possible Guesses that would be eliminated for each possible grade.
+	 * @param 	guess	Possible Guess to check for eliminations.
+	 * @return	int Number of Guesses that would be eliminated for each possible
+	 * 			grade.
+	 */
 	private int count_elim(Guess guess) {
 
 		
@@ -90,40 +113,66 @@ public class MasterAlg {
 		while(iter.hasNext()) {
 			Guess next = iter.next();
 
-			// try every grade
+			
 			for(int pos = 0; pos < NUM_POSITIONS; ++pos) {
 				for(int col = 0; col < NUM_POSITIONS - pos; ++col) {
 
 					int[] grade = {pos, col};
 
-					// if a possibility is true, it would give the given grade to the curr
-					if(!equal(grade, calc_grade(next, guess))) {
+					if(!equal_grade(grade, calc_grade(next, guess))) {
 						count++;
-					}
-				}
-			}
-		}
+					} // if next would be eliminated
+				} // for every color grade
+			} // for every position grade
+		} // while iterator has next
 		
 		return count;
 	}
 
+	/*
+	 * Instantiate the set of all possible moves using
+	 * a recursive helper method.
+	 */
 	private void instantiate_set() {
 		instantiate_set(0, new int[NUM_POSITIONS]);
 	}
 
+	/*
+	 * Instantiates the set of all possible Guesses recursively so
+	 * that set contains all possible code permutations.
+	 * Parameters: 	Index in the code array and the array that is
+	 * 				being constructed.
+	 */
 	private void instantiate_set(int index, int[] possible_code) {
+		
+		// base case when code array is full
 		if(index == NUM_POSITIONS) {
 			set.add(new Guess(possible_code));
 			return;
 		}
 
+		// Add a pin of each color to the next position
 		for(int i = 0; i < NUM_COLORS; ++i) {
-			int[] new_code = possible_code.clone();
+			
+			int[] new_code = new int[NUM_POSITIONS];
+			for(int j = 0; j < possible_code.length; ++j) {
+				new_code[j] = possible_code[j];
+			}
+			
 			new_code[index] = i;
 			instantiate_set(index + 1, new_code);
 		}
 	}
 
+	/*
+	 * Calculates the grade given a Guess possibility and a Guess
+	 * target that is assumed to be the answer. 
+	 * Parameters: Guess possibility and Guess target
+	 * Return: 	int[] grade where grade[0] is the number of pins in
+	 * 			the correct position with the correct color and
+	 * 			grade[1] is the number of pins with only the
+	 * 			correct color.
+	 */
 	private int[] calc_grade(Guess possibility, Guess target) {
 
 		int corr_pos = 0;
@@ -136,32 +185,36 @@ public class MasterAlg {
 		int[] answer = target_copy.get_code();
 
 
-		// counts correct color & position once and overwrite them with used chars
+		// correct color and position
 		for(int i = 0; i < NUM_POSITIONS; ++i) {
 			if(guess[i] == answer[i]) {
 				corr_pos++;
 				answer[i] = USED_ANSWER;
 				guess[i] = USED_GUESS;
-			}
-		}
+			} // if same position and color
+		} // for every position in the array
 
-		// counts correct color if not correct position
+		// correct color only
 		for(int i = 0; i < NUM_POSITIONS; ++i) {
 			for(int j = 0; j < NUM_POSITIONS; ++j) {
 				if(guess[i] == answer[j]) {
 					corr_color++;
 					answer[j] = USED_ANSWER;
 					guess[i] = USED_GUESS;
-				}
-			}
-		}
+				} // if same color
+			} // for every position in target
+		} // for every target in guess
 		
-
 		return new int[] {corr_pos, corr_color};
 
 	}
 
-	private boolean equal(int[] first_grade, int[] second_grade) {
+	/*
+	 * Checks if the values in two int arrays are equal
+	 * Return: 	True if the grade arrays are identical, 
+	 * 			false otherwise.
+	 */
+	private boolean equal_grade(int[] first_grade, int[] second_grade) {
 		return first_grade[0] == second_grade[0] && first_grade[1] == second_grade[1];
 	}
 
